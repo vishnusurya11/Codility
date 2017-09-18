88a43032bfea7a3701bdfbc402597dcd8b96efba
vishnusurya11 <vnandi@ucsc.edu>
2017-09-18 15:06:09 -0700

    Update FrogJmp.cpp

--- 3. Time Complexity/FrogJmp.cpp
+++ 3. Time Complexity/FrogJmp.cpp
@@ -1,1 +1,16 @@
+// you can use includes, for example:
+// #include <algorithm>
 
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int X, int Y, int D) {
+    // write your code in C++14 (g++ 6.2.0)
+ 
+   if( (Y-X)%D == 0){
+       return (Y-X)/D;
+       }
+    else{
+        return ((Y-X)/D )+1;
+        }
+}
