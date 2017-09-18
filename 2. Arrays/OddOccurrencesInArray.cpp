b1f31de0f3b1e3b550c94d292eb1b8eeb7de830a
vishnusurya11 <vnandi@ucsc.edu>
2017-09-18 13:23:51 -0700

    Update OddOccurrencesInArray.cpp

--- 2. Arrays/OddOccurrencesInArray.cpp
+++ 2. Arrays/OddOccurrencesInArray.cpp
@@ -1,1 +1,19 @@
+// you can use includes, for example:
+// #include <algorithm>
 
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int unpaired(0);
+    int vector_size(A.size());
+    
+    for(int i = 0; i < vector_size; i++){
+        unpaired = unpaired ^ A[i];                    //when you XOR two same numbers you get 0, 0 XORed with any int gives the same int
+        
+        }
+    
+    
+    return unpaired;
+}
