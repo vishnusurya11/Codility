e2f7d5064f7a3fc857e83f7d1ff9bd4fc5cf6682
vishnusurya11 <vnandi@ucsc.edu>
2017-09-18 13:16:03 -0700

    Update BinaryGap.cpp

--- 1. Iterations/BinaryGap.cpp
+++ 1. Iterations/BinaryGap.cpp
@@ -1,6 +1,6 @@
 // you can use includes, for example:
 // #include <algorithm>
-#include <cmath>
+#include <cmath>                    // to use ceil and log2 we need cmath library
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
@@ -9,10 +9,10 @@ int solution(int N) {
     int binarygap(0),max_binarygap(0);
     int base(1);
     int start_count(0);
-    int loop(ceil(log2(N)));
+    int loop(ceil(log2(N)));      //gives us the number of bits required to represent the N
     
     for(int i=0; i < loop ; i++){
-        if((N & base) == 1){
+        if((N & base) == 1){                   //checks whether the last bit of N is 1 or not
             if(start_count == 1){
                 start_count=0;
                 if(binarygap > max_binarygap){
@@ -20,7 +20,7 @@ int solution(int N) {
                     }
                 binarygap=0;
                 }
-            if(start_count == 0){
+            if(start_count == 0){              
                 start_count=1;        
                 }   
             }
@@ -29,7 +29,7 @@ int solution(int N) {
                 binarygap=binarygap+1;
                 }
             }    
-        N = N >> 1;
+        N = N >> 1;                     // right shifts N by 1
         }
     
     
