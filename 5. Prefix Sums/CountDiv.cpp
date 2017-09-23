71b61dd17fd28d45b9bd6541896f7327c7186893
vishnusurya11 <vnandi@ucsc.edu>
2017-09-23 12:17:06 -0700

    Update CountDiv.cpp

--- 5. Prefix Sums/CountDiv.cpp
+++ 5. Prefix Sums/CountDiv.cpp
@@ -7,13 +7,16 @@
 int solution(int A, int B, int K) {
     // write your code in C++14 (g++ 6.2.0)
     int Count_Div(0),num(A);
-    
+   
+    if(B < K) return 0;
+   
     while( num <= B){
         if(num % K == 0){
             Count_Div=Count_Div+1;
         }else{
-            num=num+(num%K);
-            Count_Div=Count_Div+1;
+            if(A < K) num = num+(K-A);
+            else num=num+(num%K);
+            if(num <= B ) Count_Div=Count_Div+1;
         }
         num=num+K;
     }
